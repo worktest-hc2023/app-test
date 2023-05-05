4bbbeec3280805b3164d35f8306bb9753534080f
jchen-uni <joy.chen@cmail.carleton.ca>
2023-05-05 15:29:11 -0400

    adding simple tests to test.js

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+// Getting Previous Answer
+describe('Example Tests for Jenkins Check', function(){
+    it('Checks if 1 + 1 = 2', (done)=>{
+        expect(1+1).to.equal(2);
+        done();
+    });
+
+    it('Checks is word = word', (done)=>{
+        expect("test").to.equal("test");
+        done();
+    });
+});
+
+describe('Guaranteed Fail Tests', function(){
+    it('Checks if 9 + 10 = 21', (done)=>{
+        expect(9+10).to.equal(21);
+        done();
+    });
+});
